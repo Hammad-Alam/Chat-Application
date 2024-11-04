ef56cda4b5017e5c8a95d4f814f08206c25fded1
Hammad-Alam <hammadalam115@gmail.com>
2024-11-04 19:42:07 +0500

    Halfway Done

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http); // Setup on backend
+const PORT = 5500;
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var messages = [
+  { name: "Hammad", message: "Hi" },
+  { name: "Daniyal", message: "Hi" },
+];
+
+// Create Get Message service
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+  io.emit("message", req.body);
+  res.sendStatus(200);
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
